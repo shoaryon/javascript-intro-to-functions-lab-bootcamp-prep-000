47806792bf9bfac5ee5022d6528f35b41551af70
thedavidgilmore00@gmail.com <thedavidgilmore00@gmail.com>
2018-08-06 00:38:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,34 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (sayHiToGrandma(string.toUppercase()) === uppercase) {
+    return 'YES INDEED!'
+  }
+}
+
+var uppercase = "HELLO!"
+uppercase.toUpperCase() === uppercase //True
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+var lowercase = "hello!"
+lowercase.toLowerCase() === lowercase //True
+
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase //False
+mixedCase.toUpperCase() === mixedCase //False
